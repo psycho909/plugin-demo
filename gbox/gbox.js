6a28025beb6fb91fe01fa60c2e8f52b93143f759
psycho909 <psychosocial909@gmail.com>
2025-04-14 20:39:57 +0800

    213123213

--- gbox/gbox.js
+++ gbox/gbox.js
@@ -1,191 +1,280 @@
-﻿(function ($) {
-    /*
-     * 動態引入CSS
-     */
-    var stylePath = "https://tw.hicdn.beanfun.com/beanfun/GamaWWW/allProducts/style/gbox/";
-    var newCssObj = document.createElement('link');
-    newCssObj.type = 'text/css';
-    newCssObj.rel = "stylesheet";
-    newCssObj.href = stylePath + 'gbox.css';
-    // newCssObj.href = './gbox.css';
-    document.head.appendChild(newCssObj);
+﻿// gbox - A lightweight vanilla JavaScript modal plugin
+// 轉換自jQuery插件版本
 
-    var open = false;
+(function () {
+	// 動態引入CSS
+	const stylePath = "https://tw.hicdn.beanfun.com/beanfun/GamaWWW/allProducts/style/gbox/";
+	const newCssObj = document.createElement("link");
+	newCssObj.type = "text/css";
+	newCssObj.rel = "stylesheet";
+	newCssObj.href = stylePath + "gbox.css";
+	// newCssObj.href = './gbox.css';
+	document.head.appendChild(newCssObj);
 
-    $pubBox = null;
-    var PopupPlugin = function (content, options) {
-        open = true;
-        /*
-         * 預設參數
-         */
-        var defaults = {
-            titleBar: null,
-            addClass: null,
-            fixedPos: true,
-            hasCloseBtn: false,
-            closeBtn: '\u00D7', //可插入HTML
-            clickBgClose: false,
-            hasActionBtn: true,
-            actionBtns: [{
-                text: '確定',
-                id: '',
-                class:'',
-                target: false,
-                targetClose: true,
-                click: function () {
-                    $.gbox.close(settings.afterClose); //網址 or Function
-                }
-            }],
-            afterClose: null, //網址 or Function
-            afterOpen: null //function
-        };
-        //合併 defaults 和 options，修改並返回 defaults
-        var settings = $.extend(defaults, options);
+	let open = false;
+	let $pubBox = null;
+	let $pubModule, $pubWrap, $pubContent, $pubTitleBar, $pubCloseBtn, $pubAction, $pubActionBtn;
 
-        /*
-         * 建立popupBox
-         */
+	// 輔助函數：擴展/合併物件 (相當於jQuery的$.extend)
+	const extend = (defaults, options) => {
+		const extended = {};
+		for (let prop in defaults) {
+			if (Object.prototype.hasOwnProperty.call(defaults, prop)) {
+				extended[prop] = defaults[prop];
+			}
+		}
+		for (let prop in options) {
+			if (Object.prototype.hasOwnProperty.call(options, prop)) {
+				extended[prop] = options[prop];
+			}
+		}
+		return extended;
+	};
 
+	// 輔助函數：檢查是否為函數
+	const isFunction = (fn) => {
+		return typeof fn === "function";
+	};
 
-        /*外容器*/
-        $pubBox = $('<div class="gbox" id="gbox"></div>').appendTo('body');
-        $pubModule = $('<div class="gbox-module"></div>').appendTo($pubBox);
-        $pubWrap = $('<div class="gbox-wrap"></div>').appendTo($pubBox);
-        /*內容區塊*/
-        $pubContent = $('<div class="gbox-content"></div>').appendTo($pubWrap);
-        //帶入內容
-        $pubContent.html(content);
+	const PopupPlugin = function (content, options) {
+		open = true;
+		// 預設參數
+		const defaults = {
+			titleBar: null,
+			addClass: null,
+			fixedPos: true,
+			hasCloseBtn: false,
+			closeBtn: "\u00D7", // 可插入HTML
+			clickBgClose: false,
+			hasActionBtn: true,
+			actionBtns: [
+				{
+					text: "確定",
+					id: "",
+					class: "",
+					target: false,
+					targetClose: true,
+					click: function () {
+						gbox.close(settings.afterClose); // 網址 or Function
+					}
+				}
+			],
+			afterClose: null, // 網址 or Function
+			afterOpen: null // function
+		};
+		// 合併 defaults 和 options，修改並返回 defaults
+		const settings = extend(defaults, options);
 
-        /*新增外層Class */
-        if (settings.addClass) {
-            $pubBox.addClass(settings.addClass);
-        }
-        /*標題列*/
-        if (settings.titleBar) {
-            $pubTitleBar = $('<div class="gbox-title"></div>').prependTo($pubWrap);
-            $pubTitleBar.html(settings.titleBar);
-        }
-        /*右上關閉按鈕 */
-        if (settings.hasCloseBtn) {
-            $pubCloseBtn = $('<button class="gbox-close"></button>').appendTo($pubWrap);
-            $pubCloseBtn.html(settings.closeBtn);
-            $pubCloseBtn.on('click', function () {
-                //close popup
-                $.gbox.close(settings.afterClose);
-            });
-        }
-        /*鎖定畫面*/
-        if (settings.fixedPos) {
-            $('body').addClass('ov-hidden');
-        }
+		// 建立popupBox
+		// 轉換：jQuery $('<div>').appendTo('body') → document.createElement + appendChild
 
-        /*點擊背景關閉*/
-        if (settings.clickBgClose) {
-            $(document).on('mouseup', function (e) {
-                var container = $('.gbox-wrap');
-                if (!container.is(e.target) && container.has(e.target).length === 0) {
-                    //close popup
-                    $.gbox.close(settings.afterClose);
-                }
-            });
-        }
-        /*按鈕區塊*/
-        if (settings.hasActionBtn) {
-            $pubAction = $('<div class="gbox-action"></div>').appendTo($pubWrap);
-            if (settings.actionBtns.length > 1) {
-                settings.actionBtns.forEach(function (actionBtn, index) {
-                    $pubActionBtn = $('<a class="gbox-btn"></a>').appendTo($pubAction);
-                    $pubActionBtn.html(actionBtn.text);
-                    if (actionBtn.id) {
-                        $pubActionBtn.prop('id', actionBtn.id);
-                    }
-                    if (actionBtn.class) {
-                        $pubActionBtn.addClass( actionBtn.class);
-                    }
-                    if (actionBtn.target && typeof actionBtn.click == 'string') {
-                        $pubActionBtn.prop('target', '_blank');
-                        if (actionBtn.targetClose){
-                            $pubActionBtn.on('click', function(){$.gbox.close()});
-                        }
-                    }
-                    if (typeof actionBtn.click == 'function') {
-                        $pubActionBtn.prop('href', 'javascript:;');
-                        $pubActionBtn.on('click', actionBtn.click);
-                    } else if (typeof actionBtn.click == 'string') {
-                        $pubActionBtn.prop('href', actionBtn.click);
-                    }
-                });
-            } else {
-                $pubActionBtn = $('<a class="gbox-btn"></a>').appendTo($pubAction);
-                $pubActionBtn.html(settings.actionBtns[0].text);
-                if (settings.actionBtns[0].id) {
-                    $pubActionBtn.prop('id', settings.actionBtns[0].id);
-                }
-                if (settings.actionBtns[0].class) {
-                    $pubActionBtn.addClass( settings.actionBtns[0].class);
-                }
-                if (settings.actionBtns[0].target && typeof settings.actionBtns[0].click == 'string') {
-                    $pubActionBtn.prop('target', '_blank');
-                    $pubActionBtn.on('click', function () { $.gbox.close() });
-                }
-                if (typeof settings.actionBtns[0].click == 'function') {
-                    $pubActionBtn.prop('href', 'javascript:;');
-                    $pubActionBtn.on('click', settings.actionBtns[0].click);
-                } else if (typeof settings.actionBtns[0].click == 'string') {
-                    $pubActionBtn.prop('href', settings.actionBtns[0].click);
-                }
-            }
-        }
-        /*一版到底*/
-        var mates = document.getElementsByTagName('meta');
-        for (var m = 0; m < mates.length; m++) {
-            var mate = mates[m];
-            if (mate.content.match(/750/)) {
-                $pubWrap[0].className += ' vp750';
-            }
-        }
+		// 外容器
+		$pubBox = document.createElement("div");
+		$pubBox.className = "gbox";
+		$pubBox.id = "gbox";
+		document.body.appendChild($pubBox);
 
-        /*開啟後Callback*/
-        if (typeof settings.afterOpen == 'function') {
-            settings.afterOpen();
-        }
-    };
+		$pubModule = document.createElement("div");
+		$pubModule.className = "gbox-module";
+		$pubBox.appendChild($pubModule);
 
-    $.fn.gbox = function (content, options) {
-        $(this).on('click', function (e) {
-            e.preventDefault();
-            if (open == false) {
-                new PopupPlugin(content, options);
-            }
-        });
-    };
+		$pubWrap = document.createElement("div");
+		$pubWrap.className = "gbox-wrap";
+		$pubBox.appendChild($pubWrap);
 
-    $.gbox = {
-        close: function (callback) {
-            open = false;
-            if ($pubBox != null) {
-                $pubBox.remove();
-                $pubBox = null;
+		// 內容區塊
+		$pubContent = document.createElement("div");
+		$pubContent.className = "gbox-content";
+		$pubWrap.appendChild($pubContent);
 
-                $('body').removeClass('ov-hidden');
-            }
-            if ($.isFunction(callback)) {
-                callback();
-            } else if (typeof callback == 'string') {
-                window.open(callback, '_self');
-            }
-        },
-        open: function (content, options) {
-            if (open == false) {
-                new PopupPlugin(content, options);
-            } else {
-                $pubBox.remove();
-                $pubBox = null;
+		// 帶入內容 - 轉換：jQuery的html()方法改為innerHTML
+		$pubContent.innerHTML = content;
 
-                $('body').removeClass('ov-hidden');
-                new PopupPlugin(content, options);
-            }
-        }
-    };
+		// 新增外層Class - 轉換：jQuery的addClass()改為classList.add()
+		if (settings.addClass) {
+			$pubBox.classList.add(settings.addClass);
+		}
 
-})(jQuery);
+		// 標題列 - 轉換：jQuery的prependTo改為insertBefore
+		if (settings.titleBar) {
+			$pubTitleBar = document.createElement("div");
+			$pubTitleBar.className = "gbox-title";
+			$pubTitleBar.innerHTML = settings.titleBar;
+			$pubWrap.insertBefore($pubTitleBar, $pubWrap.firstChild);
+		}
+
+		// 右上關閉按鈕 - 轉換：jQuery的on('click')改為addEventListener
+		if (settings.hasCloseBtn) {
+			$pubCloseBtn = document.createElement("button");
+			$pubCloseBtn.className = "gbox-close";
+			$pubCloseBtn.innerHTML = settings.closeBtn;
+			$pubWrap.appendChild($pubCloseBtn);
+
+			$pubCloseBtn.addEventListener("click", function () {
+				// close popup
+				gbox.close(settings.afterClose);
+			});
+		}
+
+		// 鎖定畫面 - 轉換：jQuery的addClass改為classList.add
+		if (settings.fixedPos) {
+			document.body.classList.add("ov-hidden");
+		}
+
+		// 點擊背景關閉 - 轉換：jQuery的on事件綁定轉為addEventListener
+		if (settings.clickBgClose) {
+			const clickOutsideHandler = function (e) {
+				if (!$pubWrap.contains(e.target) && e.target !== $pubWrap) {
+					// close popup
+					gbox.close(settings.afterClose);
+					// 關閉後移除事件監聽器
+					document.removeEventListener("mouseup", clickOutsideHandler);
+				}
+			};
+			document.addEventListener("mouseup", clickOutsideHandler);
+		}
+
+		// 按鈕區塊 - 轉換：jQuery的forEach與DOM操作
+		if (settings.hasActionBtn) {
+			$pubAction = document.createElement("div");
+			$pubAction.className = "gbox-action";
+			$pubWrap.appendChild($pubAction);
+
+			if (settings.actionBtns.length > 1) {
+				settings.actionBtns.forEach(function (actionBtn) {
+					const btn = document.createElement("a");
+					btn.className = "gbox-btn";
+					btn.innerHTML = actionBtn.text;
+
+					if (actionBtn.id) {
+						btn.id = actionBtn.id;
+					}
+
+					if (actionBtn.class) {
+						actionBtn.class.split(" ").forEach((cls) => {
+							if (cls) btn.classList.add(cls);
+						});
+					}
+
+					if (actionBtn.target && typeof actionBtn.click === "string") {
+						btn.target = "_blank";
+						if (actionBtn.targetClose) {
+							btn.addEventListener("click", function () {
+								gbox.close();
+							});
+						}
+					}
+
+					if (typeof actionBtn.click === "function") {
+						btn.href = "javascript:;";
+						btn.addEventListener("click", actionBtn.click);
+					} else if (typeof actionBtn.click === "string") {
+						btn.href = actionBtn.click;
+					}
+
+					$pubAction.appendChild(btn);
+				});
+			} else {
+				const btn = document.createElement("a");
+				btn.className = "gbox-btn";
+				btn.innerHTML = settings.actionBtns[0].text;
+
+				if (settings.actionBtns[0].id) {
+					btn.id = settings.actionBtns[0].id;
+				}
+
+				if (settings.actionBtns[0].class) {
+					settings.actionBtns[0].class.split(" ").forEach((cls) => {
+						if (cls) btn.classList.add(cls);
+					});
+				}
+
+				if (settings.actionBtns[0].target && typeof settings.actionBtns[0].click === "string") {
+					btn.target = "_blank";
+					btn.addEventListener("click", function () {
+						gbox.close();
+					});
+				}
+
+				if (typeof settings.actionBtns[0].click === "function") {
+					btn.href = "javascript:;";
+					btn.addEventListener("click", settings.actionBtns[0].click);
+				} else if (typeof settings.actionBtns[0].click === "string") {
+					btn.href = settings.actionBtns[0].click;
+				}
+
+				$pubAction.appendChild(btn);
+			}
+		}
+
+		// 一版到底 - 轉換：使用ES6語法重寫meta查詢
+		const metas = document.getElementsByTagName("meta");
+		for (let m = 0; m < metas.length; m++) {
+			const meta = metas[m];
+			if (meta.content.match(/750/)) {
+				$pubWrap.classList.add("vp750");
+				break;
+			}
+		}
+
+		// 開啟後Callback - 轉換：保持相同邏輯，使用isFunction輔助函數
+		if (isFunction(settings.afterOpen)) {
+			settings.afterOpen();
+		}
+	};
+
+	// 建立gbox物件作為window全局對象，取代jQuery的$.gbox
+	const gbox = {
+		close: function (callback) {
+			open = false;
+			if ($pubBox !== null) {
+				$pubBox.remove(); // 轉換：原生JS沒有remove方法，在下方定義Element.prototype.remove
+				$pubBox = null;
+				document.body.classList.remove("ov-hidden");
+			}
+
+			if (isFunction(callback)) {
+				callback();
+			} else if (typeof callback === "string") {
+				window.open(callback, "_self");
+			}
+		},
+
+		open: function (content, options) {
+			if (open === false) {
+				new PopupPlugin(content, options);
+			} else {
+				if ($pubBox !== null) {
+					$pubBox.remove();
+					$pubBox = null;
+					document.body.classList.remove("ov-hidden");
+				}
+				new PopupPlugin(content, options);
+			}
+		}
+	};
+
+	// 增強DOM元素，添加jQuery中使用的方法
+	// 為Element添加remove方法，模擬jQuery的remove()
+	if (!("remove" in Element.prototype)) {
+		Element.prototype.remove = function () {
+			if (this.parentNode) {
+				this.parentNode.removeChild(this);
+			}
+		};
+	}
+
+	// 為HTMLElement添加gbox方法，替代jQuery的$.fn.gbox
+	HTMLElement.prototype.gbox = function (content, options) {
+		this.addEventListener("click", function (e) {
+			e.preventDefault();
+			if (open === false) {
+				new PopupPlugin(content, options);
+			}
+		});
+		return this; // 支持鏈式調用
+	};
+
+	// 將gbox暴露為全局對象
+	window.gbox = gbox;
+})();
